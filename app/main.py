9b5cf4127b94baad8a115a038c1e9fef4ad6cff9
Преподаватель Топ <prep@step.lan>
2024-03-21 21:54:33 +0700

    m

--- app/main.py
+++ app/main.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI, Request, Form
+from fastapi import FastAPI, Request, status
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 import json
 from pydantic import BaseModel
 import uvicorn
+from fastapi.responses import RedirectResponse
 
 app = FastAPI() # <- Создаем экземпляр класса FastAPI
 templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 class DatabaseJson:
     def __init__(self, name_db) -> None:
@@ -28,6 +31,7 @@ database = DatabaseJson('database.json')
 @app.get('/tasks/')
 def get_tasks(request:Request):
     data = database.read()
+    data['host'] = HOST
     return templates.TemplateResponse(request=request, name='tasks.html', context=data)
 
 @app.get('/tasks/create/')
@@ -38,17 +42,24 @@ class Task(BaseModel):
     title:str
     description:str
 
+@app.post('/tasks/create/')
+def task_create(request:Request, task:Task):
+    tasks = database.read()
+    tasks['count_action']['action_tasks'] += 1
+    new_task = {
+        "id": tasks['count_action']['action_tasks'],
+        "title": task.title,
+        "description": task.description
+    }
+    tasks['tasks'].append(new_task)
+    database.write(tasks)
+    return RedirectResponse(url='/tasks/', status_code=status.HTTP_302_FOUND)
 
-@app.post('/tasks/')
-def post_task(request:Request, task:Task):
-    print(task)
-    data = database('database.json', 'r')
-    return templates.TemplateResponse(request=request, name='tasks.html', context=data)
 
 
-# uvicorn main:app --reload
+HOST = '127.0.0.1'
 
 if __name__ == '__main__':
     print('Starting server')
-    uvicorn.run('main:app', port=8000, reload=True)
+    uvicorn.run('main:app', port=8000, host=HOST, reload=True)
     print('Server stopped')
